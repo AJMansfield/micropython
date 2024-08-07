ec382057d175f3a712aa4fa822bbb2851862d685
Anson Mansfield <amansfield@mantaro.com>
2024-08-07 11:31:40 -0400

    tests/basic/class_setname: Add more tests of __set_name__.
    Signed-off-by: Anson Mansfield <amansfield@mantaro.com>

--- tests/basics/class_setname.py
+++ tests/basics/class_setname.py
@@ -21,3 +21,40 @@ class A:
 class B:
     a = A()
 
+
+
+# Test whether a class member can remove itself from the class
+class Vanish:
+    def __set_name__(self, owner, name):
+        delattr(owner, name)
+
+class Houdini:
+    trick = Vanish()
+
+houdini = Houdini()
+
+try:
+    print("attr", houdini.trick)
+except AttributeError:
+    print("attr deleted")
+
+
+# Test whether a class member can add other members to the class
+class Cat_In_Hat:
+    def __set_name__(self, owner, name):
+        owner.thing1 = Thing(1)
+        owner.thing2 = Thing(2)
+
+class Thing:
+    def __init__(self, key):
+        self.key = key
+    def __get__(self, instance, owner=None):
+        return self.key
+
+class House:
+    cat = Cat_In_Hat()
+
+house = House()
+print("thing1", house.thing1)
+print("thing2", house.thing2)
+
