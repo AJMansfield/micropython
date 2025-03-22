2b790701453387012a436ca61d5613304d5e8460
Anson Mansfield <amansfield@mantaro.com>
2025-03-22 14:52:50 -0400

    modtime: Change MICROPY_EXTMOD_TIME_FLOAT to MICROPY_PY_TIME_FLOAT.
    Signed-off-by: Anson Mansfield <amansfield@mantaro.com>

--- extmod/modtime.h
+++ extmod/modtime.h
@@ -29,6 +29,14 @@
 
 #include "py/obj.h"
 
+// Whether to use floating point output for `time.time()`.
+// Single-precision floats are not adequate; REPR_C floats can't represent whole numbers precisely
+// past 4194304.0, i.e. any unix timestamp later than Feb 18, 1970. Double precision is needed
+// for floating point output to be meaningfully useful.
+#ifndef MICROPY_PY_TIME_FLOAT
+#define MICROPY_PY_TIME_FLOAT (MICROPY_CPYTHON_COMPAT ? (MICROPY_PY_BUILTINS_FLOAT && MICROPY_FLOAT_IMPL >= MICROPY_FLOAT_IMPL_DOUBLE) : 0)
+#endif
+
 MP_DECLARE_CONST_FUN_OBJ_1(mp_time_mktime_obj);
 MP_DECLARE_CONST_FUN_OBJ_1(mp_time_sleep_obj);
 MP_DECLARE_CONST_FUN_OBJ_1(mp_time_sleep_ms_obj);
