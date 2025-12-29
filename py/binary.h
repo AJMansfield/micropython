41ccce5e44280a000da689e1c375dde7273ef5af
Anson Mansfield <amansfield@mantaro.com>
2025-12-29 11:36:24 -0500

    py/binary: Reorganize and give each typecode a named macro.
    Signed-off-by: Anson Mansfield <amansfield@mantaro.com>

--- py/binary.h
+++ py/binary.h
@@ -27,41 +27,66 @@
 #ifndef MICROPY_INCLUDED_PY_BINARY_H
 #define MICROPY_INCLUDED_PY_BINARY_H
 
+#include <stdint.h>
+
 #include "py/obj.h"
 
-// Use special typecode to differentiate repr() of bytearray vs array.array('B')
+// Basic typecodes
+#define MP_TYPECODE_CHAR 'b'
+#define MP_TYPECODE_INT8 'b'
+#define MP_TYPECODE_UINT8 'B'
+#define MP_TYPECODE_INT16 'h'
+#define MP_TYPECODE_UINT16 'H'
+#define MP_TYPECODE_INT32 'i'
+#define MP_TYPECODE_UINT32 'I'
+#define MP_TYPECODE_INT64 'l'
+#define MP_TYPECODE_UINT64 'L'
+#define MP_TYPECODE_INT128 'q'
+#define MP_TYPECODE_UINT128 'Q'
+
+#define MP_TYPECODE_FLOAT16 'e'
+#define MP_TYPECODE_FLOAT32 'f'
+#define MP_TYPECODE_FLOAT64 'd'
+
+// "Unsafe" typecodes used by unix ffi and struct extensions
+#define MP_TYPECODE_OBJECT 'O'
+#define MP_TYPECODE_CALLBACK 'C'
+#define MP_TYPECODE_STRING 'S'
+#define MP_TYPECODE_STRING_CONST 's'
+#define MP_TYPECODE_POINTER 'P'
+#define MP_TYPECODE_POINTER_CONST 'p'
+#define MP_TYPECODE_VOID 'v'
+
+// Special typecode to differentiate repr() of bytearray vs array.array('B')
 // (underlyingly they're same).  Can't use 0 here because that's used to detect
 // type-specification errors due to end-of-string.
 #define MP_TYPECODE_BYTEARRAY 1
-#define MP_TYPECODE_OBJECT 'O'
-#define MP_TYPECODE_CALLBACK 'C'
-#define MP_TYPECODE_VOID 'v'
-#define MP_TYPECODE_HALFFLOAT 'e'
 
-// array/memoryview of the given C type
-#define MP_TYPECODE_C(type) _Generic((type)0, \
-    char : 'b', \
-    signed char : 'b', \
-    unsigned char : 'B', \
-    short : 'h', \
-    unsigned short : 'H', \
-    int : 'i', \
-    unsigned int : 'I', \
-    long : 'l', \
-    unsigned long : 'L', \
-    long long : 'q', \
-    unsigned long long : 'Q', \
-    float : 'f', \
-    double : 'd', \
-    char _MP_TYPECODES_UNCRUSTIFY_WORKAROUND_STAR: 'S', \
-    const char _MP_TYPECODES_UNCRUSTIFY_WORKAROUND_STAR: 's', \
-    void _MP_TYPECODES_UNCRUSTIFY_WORKAROUND_STAR: 'P', \
-    const void _MP_TYPECODES_UNCRUSTIFY_WORKAROUND_STAR: 'p' \
-    )
-#define _MP_TYPECODES_UNCRUSTIFY_WORKAROUND_STAR *
-
-// Used only for memoryview types, set in "typecode" to indicate a writable memoryview
+// Used only for memoryview types; OR with the typecode value to indicate a writable memoryview
 #define MP_TYPECODE_FLAG_RW (0x80)
+
+/*UNCRUSTIFY-OFF*/
+// Helper macro to get typecode from C expression type
+#define MP_TYPECODE_C(expr) _Generic(expr, \
+    char : MP_TYPECODE_CHAR, \
+    int8_t: MP_TYPECODE_INT8, \
+    uint8_t: MP_TYPECODE_UINT8, \
+    int16_t: MP_TYPECODE_INT16, \
+    uint16_t: MP_TYPECODE_UINT16, \
+    int32_t: MP_TYPECODE_INT32, \
+    uint32_t: MP_TYPECODE_UINT32, \
+    int64_t: MP_TYPECODE_INT64, \
+    uint64_t: MP_TYPECODE_UINT64, \
+    long long : MP_TYPECODE_INT128, \
+    unsigned long long : MP_TYPECODE_UINT128, \
+    float : MP_TYPECODE_FLOAT32, \
+    double : MP_TYPECODE_FLOAT64, \
+    char *: MP_TYPECODE_STRING, \
+    const char *: MP_TYPECODE_STRING_CONST, \
+    void *: MP_TYPECODE_POINTER, \
+    const void *: MP_TYPECODE_POINTER_CONST)
+/*UNCRUSTIFY-ON*/
+
 
 size_t mp_binary_get_size(char struct_type, char val_type, size_t *palign);
 mp_obj_t mp_binary_get_val_array(char typecode, void *p, size_t index);
